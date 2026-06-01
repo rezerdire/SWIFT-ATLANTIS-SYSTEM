f93247f15a02b1bd7df836e3f9b26e30bff8d947
Christian Vacaro <vacaro.267597@gmail.com>
2026-06-01 18:46:24 +0800

    api vercel config

--- api/lambda.php
+++ api/lambda.php
@@ -5,22 +5,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
+// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+// Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
-// Trust Vercel's load balancer so Laravel detects HTTPS correctly
-Request::setTrustedProxies(
-    ['*'],
-    Request::HEADER_X_FORWARDED_FOR |
-    Request::HEADER_X_FORWARDED_HOST |
-    Request::HEADER_X_FORWARDED_PORT |
-    Request::HEADER_X_FORWARDED_PROTO
-);
 
 $app->handleRequest(Request::capture());
